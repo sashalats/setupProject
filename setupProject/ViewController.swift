df77a751dc3a77e4e2deb534603670b8a34fe6a0
Alexander <sasha.lats@icloud.com>
2024-12-20 10:20:57 +0300

    Initial Commit

---
+++ setupProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  setupProject
+//
+//  Created by sashalats on 20.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ setupProjectTests/setupProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  setupProjectTests.swift
+//  setupProjectTests
+//
+//  Created by sashalats on 20.12.2024.
+//
+
+import Testing
+@testable import setupProject
+
+struct setupProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
